bd5dcd2864dc373d71600d61acd949dc74cd3f6c
Saadat-MBP16 <me@saadat.dev>
2020-07-16 23:13:05 +0200

    Add Instruction Annotation --> Mainly added for clearer code
    Signed-off-by: Saadat-MBP16 <me@saadat.dev>

---
+++ src/main/java/de/saadatbaig/J6502/Utils/Annotations/Instruction.java
@@ -0,0 +1,12 @@
+package de.saadatbaig.J6502.Utils.Annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.SOURCE)
+public @interface Instruction {
+    int opcode();
+}
